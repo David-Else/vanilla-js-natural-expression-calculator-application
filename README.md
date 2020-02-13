f3e500a4cfe6f8bf750c2863145cfc2c5f00c77e
David <12832280+David-Else@users.noreply.github.com>
2020-02-13 10:06:10 +0000

    Clean up and add README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Natural Expression Calculator Application
+
+Original version of the app written in vanilla JavaScript. Tests are being re-written in deno and don't work yet.
+
+Live on the [Natural Expression](https://naturalexpression.co.uk/) website home page.
--- src/view.js
+++ src/view.js
@@ -3,7 +3,7 @@ const genderBoxClassColor = `natural-expression-generator__gender-box--color-`;
 /*
  * Read from the DOM
  */
-export const genderBox = document.getElementById("js-gender-box");
+export const genderBox = document.getElementById('js-gender-box');
 
 /*
  * Write to the DOM
@@ -12,60 +12,31 @@ export function changeDisplayProperty(elementId, value) {
   document.getElementById(elementId).style.display = value;
 }
 
-// test
-// https://ultimatecourses.com/blog/stop-toggling-classes-with-js-use-behaviour-driven-dom-manipulation-with-data-states
-
-// .nav ul[data-state=closed] {
-//   display: none;
-// }
-// .nav ul[data-state=open] {
-//   display: inherit;
-// }
-
-// var toggleState = function(elem, one, two) {
-//   var elem = document.querySelector(elem);
-//   elem.setAttribute(
-//     "data-state",
-//     elem.getAttribute("data-state") === one ? two : one
-//   );
-//   console.log(elem.getAttribute("data-state"));
-// };
-
-// genderBox.onclick = function(e) {
-//   toggleState(
-//     `.natural-expression-generator__gender-box--color-female`,
-//     "closed",
-//     "open"
-//   );
-//   e.preventDefault();
-// };
-///
-
 // OR just use toggle? or have i been through all that...
 export function setGenderBoxMale() {
-  genderBox.textContent = "Male";
+  genderBox.textContent = 'Male';
   genderBox.classList.remove(`${genderBoxClassColor}female`);
   genderBox.classList.add(`${genderBoxClassColor}male`);
 }
 
 export function switchGenderBoxFemale() {
-  genderBox.textContent = "Female";
+  genderBox.textContent = 'Female';
   genderBox.classList.remove(`${genderBoxClassColor}male`);
   genderBox.classList.add(`${genderBoxClassColor}female`);
 }
 
 export function initGenderBox() {
-  genderBox.textContent = "Male";
+  genderBox.textContent = 'Male';
   genderBox.classList.add(`${genderBoxClassColor}male`);
 }
 
 export function outputAttempsLeft() {
-  document.getElementById("attemps-left").innerHTML = localStorage.triesLeft;
+  document.getElementById('attemps-left').innerHTML = localStorage.triesLeft;
 }
 
 export function outputResults(results) {
   outputAttempsLeft();
-  document.getElementById("results").innerHTML = `
+  document.getElementById('results').innerHTML = `
   <p>Your Expression is <strong>${results.typeOfExpression}</strong></p>
   <p>You are <strong>${results.duality}</strong></p>
   <p>You are a <strong>${results.complexity}</strong> Expression</p>
